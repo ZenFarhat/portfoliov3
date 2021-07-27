60a0fb5a637da290781dbf6a0045c2d116584293
Zen F <zeinfarhat6@gmail.com>
2021-07-27 16:53:20 +1000

    styling and animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,11 +14,10 @@ function Hero() {
           </h1>
         </div>
         <h3 className='hero__desc hero__text'>
-          A dedicated self-driven{' '}
-          <span className='base__span'>frontend software developer</span> with a
-          strong passion for building <br />
-          responsive, performant & modern web applications with great user{' '}
-          <br /> experiences that positively impact the world.
+          Dedicated, <span className='base__span'>frontend developer </span>
+          known for designing, developing, and delivering custom web
+          applications and software solutions. Applies a variety of front-end
+          technologies in diverse website development projects.
         </h3>
         <div className='hero__icons hero__text'>
           <a
