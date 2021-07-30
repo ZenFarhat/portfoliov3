e34df8bf6ea73977d3643214d081a25c9cfcb401
Zen F <zeinfarhat6@gmail.com>
2021-07-30 20:52:36 +1000

    animations added

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import '../../styles/style.css'
+import Fade from 'react-reveal/Fade'
 
 function Project(props) {
   return (
-    <div
-      className='project'
-      style={{ backgroundImage: 'url(' + props.img + ')' }}
-    >
-      <div className={`project__desc project__${props.colour}`}>
-        <h3 className='project__name'>{props.name}</h3>
-        <div className='project__divider'></div>
-        <p className='project__techstack'>{`Technologies used: ${props.tech}`}</p>
-        <div className='project__buttons'>
-          <a href={props.deployLink} target='_blank' rel='noreferrer'>
-            <button>View Deployment</button>
-          </a>
-          <a href={props.codeLink} target='_blank' rel='noreferrer'>
-            <button>View Code</button>
-          </a>
+    <Fade>
+      <div
+        className='project'
+        style={{ backgroundImage: 'url(' + props.img + ')' }}
+      >
+        <div className={`project__desc project__${props.colour}`}>
+          <h3 className='project__name'>{props.name}</h3>
+          <div className='project__divider'></div>
+          <p className='project__techstack'>{`Technologies used: ${props.tech}`}</p>
+          <div className='project__buttons'>
+            <a href={props.deployLink} target='_blank' rel='noreferrer'>
+              <button>View Deployment</button>
+            </a>
+            <a href={props.codeLink} target='_blank' rel='noreferrer'>
+              <button>View Code</button>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </Fade>
   )
 }
 
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../../styles/style.css'
+import Rotate from 'react-reveal/Rotate'
 
 function Skills() {
   const skills = [
-    { text: 'HTML', class: 'fab fa-html5 skill-icon html-icon' },
+    { text: 'HTML', class: 'fab fa-html5 skill-icon html-icon ' },
     { text: 'CSS', class: 'fab fa-css3-alt skill-icon css-icon' },
-    { text: 'JS', class: 'fab fa-js-square skill-icon js-icon' },
+    { text: 'JS', class: 'fab fa-js-square skill-icon js-icon ' },
     { text: 'React', class: 'fab fa-react skill-icon react-icon' },
     { text: 'Angular', class: 'fab fa-angular skill-icon angular-icon' },
     { text: 'Sass', class: 'fab fa-sass skill-icon sass-icon' },
@@ -18,10 +19,12 @@ function Skills() {
       <div className='skills__container'>
         {skills.map((skill) => {
           return (
-            <div className='skill__card'>
-              <i class={skill.class}></i>
-              <p className='skill__text'>{skill.text}</p>
-            </div>
+            <Rotate>
+              <div className='skill__card'>
+                <i class={skill.class}></i>
+                <p className='skill__text'>{skill.text}</p>
+              </div>
+            </Rotate>
           )
         })}
       </div>
